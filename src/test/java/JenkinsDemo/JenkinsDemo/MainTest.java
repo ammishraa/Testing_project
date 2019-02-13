0247635588a8048f2b741ffb2f9641e2d3714840
Ashutosh Mishra <amishra269@asiapac.globalcsc.net>
2019-02-13 19:56:29 +0530

    initial commit

---
+++ src/test/java/JenkinsDemo/JenkinsDemo/MainTest.java
@@ -0,0 +1,20 @@
+package JenkinsDemo.JenkinsDemo;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+	@Test
+	public void testOne() {
+		System.out.println("testOne");
+	}
+
+	@Test
+	public void testTwo() {
+		System.out.println("testTwo");
+	}
+
+	@Test
+	public void testThree() {
+		System.out.println("testThree");
+	}
+}
